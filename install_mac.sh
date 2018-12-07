889edda34498147c5e0861d4824ef0052a61284c
Brent Maxwell <theheretic@gmail.com>
2018-12-07 15:39:45 +1100

    Add files via upload

---
+++ install_mac.sh
@@ -0,0 +1,26 @@
+#/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#brew tap homebrew/cask-versions
+#brew tap caskroom/fonts
+#brew cask install dropbox
+brew cask install avibrazil-rdm
+brew cask install 1password
+brew cask install pycharm
+brew cask install spotify
+brew cask install transmission
+brew cask install etcher
+#brew cask install firefox
+brew cask install gimp
+brew cask install vlc
+#brew cask install adobe-acrobat-reader
+#brew cask install font-source-code-pro
+brew cask install amazon-workspaces
+brew cask install amazon-workdocs
+brew cask install whatsapp
+brew install python
+brew install mas
+mas install 417375580 #BetterSnapTool
+mas install 497799835 #XCode
+mas install 1295203466 #Microsoft Remote Desktop 10
+mas install 417402227 #CalcMadeEasy
+mas install 803453959 #Slack
+mas install 406056744 #Evernote
